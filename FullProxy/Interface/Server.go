c7e7e3874e6a24af81f1819401da9c9b9573939f
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-07-26 18:22:22 -0500

    Update Server.go

--- FullProxy/Interface/Server.go
+++ FullProxy/Interface/Server.go
@@ -31,24 +31,25 @@ func startProxying(clientConnection net.Conn, targetConnection net.Conn){
 	Basic.Proxy(
 		clientConnection, targetConnection,
 		clientConnectionReader, clientConnectionWriter,
-		targetConnectionReader, targetConnectionWriter, true)
-	_ = clientConnection.Close()
-	_ = targetConnection.Close()
+		targetConnectionReader, targetConnectionWriter, false)
 }
 
 
 func Server(address string, port string){
+	log.Print("Starting Interface server")
 	server, BindingError  := net.Listen("tcp", address + ":" + port)
 	if BindingError != nil {
 		log.Print("Something goes wrong: " + BindingError.Error())
 		return
 	}
 	log.Printf("Bind successfully in %s:%s", address, port)
+	log.Print("Waiting for proxy server connections...")
 	masterConnection, connectionError := server.Accept()
 	if connectionError != nil{
 		_ = server.Close()
 		return
 	}
+	log.Print("Reverse connection received from: ", masterConnection.RemoteAddr())
 	setupControlCSignal(server, masterConnection)
 	masterConnectionWriter := bufio.NewWriter(masterConnection)
 	for {
