8b7fa9add67a048c807a36b8de06632fae2b4a82
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-07-26 14:17:56 -0500

    Update HashAlgorithms.go

--- CryptoTools/HashAlgorithms.go
+++ CryptoTools/HashAlgorithms.go
@@ -10,17 +10,17 @@ func Sha3_512_256(buffer []byte) []byte{
 }
 
 
-func GetPasswordHashPasswordByteArray(username string, password []byte) []byte{
-	if len(username) > 0{
-		return Sha3_512_256(password)
+func GetPasswordHashPasswordByteArray(username *[]byte, password *[]byte) []byte{
+	if len(*username) > 0{
+		return Sha3_512_256(*password)
 	}
 	return nil
 }
 
 
-func GetPasswordHashPasswordString(username string, password string) []byte{
-	if len(username) > 0{
-		return Sha3_512_256([]byte(password))
+func GetPasswordHashPasswordString(username *string, password *string) []byte{
+	if len(*username) > 0{
+		return Sha3_512_256([]byte(*password))
 	}
 	return nil
 }
