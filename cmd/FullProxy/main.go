0432f0caecb83949653a2f17233599a2e6c7365c
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-07-26 14:27:26 -0500

    Create main.go

---
+++ cmd/FullProxy/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"FullProxy/cmd/FullProxy/ArgumentParser"
+	"FullProxy/cmd/FullProxy/Proxies/SOCKS5"
+	"fmt"
+	"os"
+)
+
+
+func main(){
+	var arguments = ArgumentParser.GetArguments()
+	switch arguments["protocol"].(string) {
+	case "socks4":
+		fmt.Println("Sock4 not implemented yet")
+	case "socks5":
+		SOCKS5.StartSocks5(arguments["ip"].(string),
+							arguments["port"].(string),
+							arguments["interface-mode"].(bool),
+							[]byte(arguments["username"].(string)),
+							[]byte(arguments["password"].(string)))
+	case "http":
+		fmt.Println("HTTP not implemented yet")
+	case "interface":
+		fmt.Println("Interface not implemented yet")
+	default:
+		_, _ = fmt.Fprint(os.Stderr, "Unknown module supplied, use \"help\"")
+		os.Exit(1)
+	}
+}
