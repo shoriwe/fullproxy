56b74713b51758bda2f012365321ae6df9de7ca7
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-07-26 15:52:23 -0500

    Update Protocol.go

--- FullProxy/Proxies/SOCKS5/Protocol.go
+++ FullProxy/Proxies/SOCKS5/Protocol.go
@@ -12,33 +12,6 @@ import (
 )
 
 
-const (
-	BasicNegotiation		byte = 1
-	Version                 byte = 5
-	NoAuthRequired          byte = 0
-	InvalidMethod			byte = 1
-	UsernamePassword		byte = 2
-	// SOCKS commands
-	Connect           byte = 1
-	Bind              byte = 2
-	UDPAssociate      byte = 3
-	// SOCKS valid address types
-	IPv4              byte = 1
-	DomainName        byte = 3
-	IPv6              byte = 4
-	// SOCKS5 responses
-	ConnectionRefused byte = 5
-	Succeeded         byte = 0
-)
-
-var (
-	// SOCKS requests connection responses
-	UsernamePasswordSupported	 = []byte{Version, UsernamePassword}
-	NoAuthRequiredSupported      = []byte{Version, NoAuthRequired}
-	NoSupportedMethods           = []byte{5, 255}
-)
-
-
 func ReceiveTargetRequest(clientConnectionReader *bufio.Reader) (byte, byte, []byte, []byte) {
 	numberOfBytesReceived, targetRequest, ConnectionError := Sockets.Receive(clientConnectionReader, 1024)
 	if ConnectionError != nil {
