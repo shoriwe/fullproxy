dd1b3aafdee93c7bf39e5bbd77c98310beedc4d8
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-07-26 18:22:17 -0500

    Update Protocol.go

--- FullProxy/Proxies/Basic/Protocol.go
+++ FullProxy/Proxies/Basic/Protocol.go
@@ -5,9 +5,31 @@ import (
 	"bufio"
 	"log"
 	"net"
-	"time"
 )
 
+
+func HandleReadWrite(
+	sourceConnection net.Conn, destinationConnection net.Conn,
+	sourceReader *bufio.Reader, destinationWriter *bufio.Writer, sniff *bool){
+	for {
+		// _ = clientConnection.SetReadDeadline(time.Now().Add(100))
+		numberOfBytesReceived, buffer, ConnectionError := Sockets.Receive(sourceReader, 20480)
+		if ConnectionError != nil {
+			if ConnectionError, ok := ConnectionError.(net.Error); !(ok && ConnectionError.Timeout()) {
+				break
+			}
+		}
+		if *sniff && numberOfBytesReceived > 0 {
+			log.Print("Sending from: ", sourceConnection.RemoteAddr(), " to: ", destinationConnection.RemoteAddr(), " Chunk: ", buffer[:numberOfBytesReceived])
+		}
+		_, ConnectionError = Sockets.Send(destinationWriter, buffer[:numberOfBytesReceived])
+		if ConnectionError != nil {
+			break
+		}
+		buffer = nil
+	}
+	_ = sourceConnection.Close()
+}
 
 func Proxy(clientConnection net.Conn,
 	targetConnection net.Conn,
@@ -16,40 +38,6 @@ func Proxy(clientConnection net.Conn,
 	targetConnectionReader *bufio.Reader,
 	targetConnectionWriter *bufio.Writer,
 	sniff bool) {
-	for {
-		for state := 0; state < 2; state++ {
-			switch state {
-			case 1:
-				_ = clientConnection.SetReadDeadline(time.Now().Add(100))
-				numberOfBytesReceived, buffer, ConnectionError := Sockets.Receive(clientConnectionReader, 20480)
-				if ConnectionError != nil {
-					if ConnectionError, ok := ConnectionError.(net.Error); !(ok && ConnectionError.Timeout()) {
-						return
-					}
-				}
-				if sniff && numberOfBytesReceived > 0 {
-					log.Print("Sending from: ", clientConnection.RemoteAddr(), " to: ", targetConnection.RemoteAddr(), " Chunk: ", buffer[:numberOfBytesReceived])
-				}
-				_, ConnectionError = Sockets.Send(targetConnectionWriter, buffer[:numberOfBytesReceived])
-				if ConnectionError != nil {
-					return
-				}
-			case 0:
-				_ = targetConnection.SetReadDeadline(time.Now().Add(100))
-				numberOfBytesReceived, buffer, ConnectionError := Sockets.Receive(targetConnectionReader, 20480)
-				if ConnectionError != nil {
-					if ConnectionError, ok := ConnectionError.(net.Error); !(ok && ConnectionError.Timeout()) {
-						return
-					}
-				}
-				if sniff && numberOfBytesReceived > 0{
-					log.Print("Sending from: ", targetConnection.RemoteAddr(), " to: ", clientConnection.RemoteAddr(), " Chunk: ", buffer[:numberOfBytesReceived])
-				}
-				_, ConnectionError = Sockets.Send(clientConnectionWriter, buffer[:numberOfBytesReceived])
-				if ConnectionError != nil {
-					return
-				}
-			}
-		}
-	}
+	go HandleReadWrite(clientConnection, targetConnection, clientConnectionReader, targetConnectionWriter, &sniff)
+	go HandleReadWrite(targetConnection, clientConnection, targetConnectionReader, clientConnectionWriter, &sniff)
 }
