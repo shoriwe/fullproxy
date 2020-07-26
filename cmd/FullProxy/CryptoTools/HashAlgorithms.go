5a8024c8ff6c860ee04fc323cad43e62a4ca7025
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-07-26 14:27:20 -0500

    Create HashAlgorithms.go

---
+++ cmd/FullProxy/CryptoTools/HashAlgorithms.go
@@ -0,0 +1,26 @@
+package CryptoTools
+
+import (
+	"crypto/sha512"
+)
+
+
+func Sha3_512_256(buffer []byte) []byte{
+	return sha512.New512_256().Sum(buffer)
+}
+
+
+func GetPasswordHashPasswordByteArray(username *[]byte, password *[]byte) []byte{
+	if len(*username) > 0{
+		return Sha3_512_256(*password)
+	}
+	return nil
+}
+
+
+func GetPasswordHashPasswordString(username *string, password *string) []byte{
+	if len(*username) > 0{
+		return Sha3_512_256([]byte(*password))
+	}
+	return nil
+}
