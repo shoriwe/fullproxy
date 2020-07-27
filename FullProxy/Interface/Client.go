d58ec978387df983dc702ae5842b8586b4d0e4ab
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-07-27 11:31:40 -0500

    Update Client.go

--- FullProxy/Interface/Client.go
+++ FullProxy/Interface/Client.go
@@ -13,31 +13,31 @@ type Function func(conn net.Conn, username *[]byte, passwordHash *[]byte)
 
 func Client(address string, port string, username *[]byte, passwordHash *[]byte, function Function){
 	log.Printf("Trying to connecto to %s:%s", address, port)
-	var clientConnection net.Conn
-	var masterConnection = Sockets.Connect(address,  port)
-	if masterConnection == nil{
-		return
-	}
-	masterConnectionReader := bufio.NewReader(masterConnection)
-	log.Printf("Successfully connected to %s:%s", address, port)
-	for {
-		NumberOfReceivedBytes, buffer, ConnectionError := Sockets.Receive(masterConnectionReader, 1024)
-		if ConnectionError != nil{
-			log.Printf("Error: %s\n", ConnectionError.Error())
-			break
+	masterConnection := Sockets.Connect(address,  port)
+	if masterConnection != nil{
+		masterConnectionReader := bufio.NewReader(masterConnection)
+		log.Printf("Successfully connected to %s:%s", address, port)
+		for {
+			NumberOfReceivedBytes, buffer, ConnectionError := Sockets.Receive(masterConnectionReader, 1024)
+			if ConnectionError == nil{
+				if NumberOfReceivedBytes == 1{
+					switch buffer[0] {
+					case Shutdown:
+						log.Print("Received shutdown signal... shutting down")
+						break
+					case NewConnection:
+						clientConnection := Sockets.Connect(address, port)
+						if clientConnection != nil{
+							go function(clientConnection, username, passwordHash)
+						}
+					}
+				} else {
+					continue
+				}
+			} else {
+				log.Printf("Error: %s\n", ConnectionError.Error())
+				break
+			}
 		}
-		if NumberOfReceivedBytes != 1{
-			continue
-		}
-		if buffer[0] == Shutdown {
-			log.Print("Received shutdown signal from the interface... Shutting down now the proxy service")
-			break
-		}
-		clientConnection = Sockets.Connect(address, port)
-		if clientConnection == nil{
-			continue
-		}
-		go function(clientConnection, username, passwordHash)
-
 	}
 }
