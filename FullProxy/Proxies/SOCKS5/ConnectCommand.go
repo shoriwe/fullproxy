a4a7550ec3226e6e69657d9fa356316d592a5cbd
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-07-27 11:07:17 -0500

    Update ConnectCommand.go

--- FullProxy/Proxies/SOCKS5/ConnectCommand.go
+++ FullProxy/Proxies/SOCKS5/ConnectCommand.go
@@ -12,25 +12,32 @@ func PrepareConnect(
 	clientConnection net.Conn, clientConnectionReader *bufio.Reader,
 	clientConnectionWriter *bufio.Writer, targetAddress *string,
 	targetPort *string, rawTargetAddress []byte,
-	rawTargetPort []byte, targetAddressType *byte) net.Conn{
+	rawTargetPort []byte, targetAddressType *byte){
 
-	var targetConnection = Sockets.Connect(*targetAddress, *targetPort) // new(big.Int).SetBytes(rawTargetPort).String())
-	if targetConnection == nil {
+	var connectionError error
+	var targetConnection net.Conn
+	targetConnection = Sockets.Connect(*targetAddress, *targetPort) // new(big.Int).SetBytes(rawTargetPort).String())
+
+	if targetConnection != nil {
+		response := []byte{Version, Succeeded, 0, *targetAddressType}
+		response = append(response[:], rawTargetAddress[:]...)
+		response = append(response[:], rawTargetPort[:]...)
+		_, connectionError = Sockets.Send(clientConnectionWriter, response)
+		if connectionError == nil {
+			targetConnectionReader := bufio.NewReader(targetConnection)
+			targetConnectionWriter := bufio.NewWriter(targetConnection)
+			Basic.Proxy(
+				clientConnection, targetConnection,
+				clientConnectionReader, clientConnectionWriter,
+				targetConnectionReader, targetConnectionWriter, false)
+		} else {
+			{
+				_ = clientConnection.Close()
+				_ = targetConnection.Close()
+			}
+		}
+	} else {
 		_, _ = Sockets.Send(clientConnectionWriter, []byte{Version, ConnectionRefused, 0, *targetAddressType, 0, 0})
-		return nil
+		_ = clientConnection.Close()
 	}
-	response := []byte{Version, Succeeded, 0, *targetAddressType}
-	response = append(response[:], rawTargetAddress[:]...)
-	response = append(response[:], rawTargetPort[:]...)
-	_, ConnectionError := Sockets.Send(clientConnectionWriter, response)
-	if ConnectionError != nil {
-		return nil
-	}
-	targetConnectionReader := bufio.NewReader(targetConnection)
-	targetConnectionWriter := bufio.NewWriter(targetConnection)
-	Basic.Proxy(
-		clientConnection, targetConnection,
-		clientConnectionReader, clientConnectionWriter,
-		targetConnectionReader, targetConnectionWriter, false)
-	return targetConnection
 }
