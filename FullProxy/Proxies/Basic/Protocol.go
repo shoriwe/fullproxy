d67d1f172c29a36a7dc027c1bcc52695f547faf3
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-07-28 10:47:11 -0500

    Update Protocol.go

--- FullProxy/Proxies/Basic/Protocol.go
+++ FullProxy/Proxies/Basic/Protocol.go
@@ -3,42 +3,44 @@ package Basic
 import (
 	"FullProxy/FullProxy/Sockets"
 	"bufio"
-	"log"
 	"net"
+	"time"
 )
 
 
 func HandleReadWrite(
-	sourceConnection net.Conn, destinationConnection net.Conn,
-	sourceReader *bufio.Reader, destinationWriter *bufio.Writer, sniff *bool){
+	sourceConnection net.Conn, sourceReader *bufio.Reader, destinationWriter *bufio.Writer, connectionAlive *bool){
 	for {
-		// _ = clientConnection.SetReadDeadline(time.Now().Add(100))
+		if !(*connectionAlive){
+			break
+		}
+		_ = sourceConnection.SetReadDeadline(time.Now().Add(10 * time.Second))
 		numberOfBytesReceived, buffer, ConnectionError := Sockets.Receive(sourceReader, 20480)
 		if ConnectionError != nil {
 			if ConnectionError, ok := ConnectionError.(net.Error); !(ok && ConnectionError.Timeout()) {
 				break
 			}
 		}
-		if *sniff && numberOfBytesReceived > 0 {
-			log.Print("Sending from: ", sourceConnection.RemoteAddr(), " to: ", destinationConnection.RemoteAddr(), " Chunk: ", buffer[:numberOfBytesReceived])
-		}
-		_, ConnectionError = Sockets.Send(destinationWriter, buffer[:numberOfBytesReceived])
-		if ConnectionError != nil {
-			break
-		}
+		if numberOfBytesReceived > 0 {
+			_, ConnectionError = Sockets.Send(destinationWriter, buffer[:numberOfBytesReceived])
+			if ConnectionError != nil {
+				break
+			}}
 		buffer = nil
 	}
 	_ = sourceConnection.Close()
+	*connectionAlive = false
 }
+
 
 func Proxy(clientConnection net.Conn,
 	targetConnection net.Conn,
 	clientConnectionReader *bufio.Reader,
 	clientConnectionWriter *bufio.Writer,
 	targetConnectionReader *bufio.Reader,
-	targetConnectionWriter *bufio.Writer,
-	sniff bool) {
+	targetConnectionWriter *bufio.Writer) {
+	connectionAlive := true
 
-	go HandleReadWrite(clientConnection, targetConnection, clientConnectionReader, targetConnectionWriter, &sniff)
-	go HandleReadWrite(targetConnection, clientConnection, targetConnectionReader, clientConnectionWriter, &sniff)
+	go HandleReadWrite(clientConnection, clientConnectionReader, targetConnectionWriter, &connectionAlive)
+	go HandleReadWrite(targetConnection, targetConnectionReader, clientConnectionWriter, &connectionAlive)
 }
