e63e88afa91e632f5ae14149d5956f6737a43890
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-08-01 17:37:40 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,27 +1,34 @@
 package main
 
 import (
-	"FullProxy/FullProxy/ArgumentParser"
-	"FullProxy/FullProxy/Interface"
-	"FullProxy/FullProxy/Proxies/SOCKS5"
 	"fmt"
+	"github.com/shoriwe/FullProxy/src/ArgumentParser"
+	"github.com/shoriwe/FullProxy/src/Interface"
+	"github.com/shoriwe/FullProxy/src/Proxies/SOCKS5"
+	"os"
 )
 
 
-func main(){
-	arguments := ArgumentParser.GetArguments()
-	if arguments.Parsed {
-		switch arguments.Protocol {
-		case "socks5":
-			SOCKS5.StartSocks5(arguments.IP,
-				arguments.Port,
-				arguments.InterfaceMode,
-				[]byte(arguments.Username),
-				[]byte(arguments.Password))
-		case "http":
-			fmt.Println("HTTP not implemented yet")
-		case "interface-master":
-			Interface.Server(arguments.IP, arguments.Port)
-		}
+func main() {
+	if len(os.Args) == 1 {
+		_, _ = fmt.Fprintln(os.Stderr, "Try: ", os.Args[0], " help")
+		os.Exit(-1)
+	}
+	switch os.Args[1] {
+	case "socks5":
+		ip, port, username, password, interfaceMode := ArgumentParser.ParseSocks5Arguments()
+		SOCKS5.StartSocks5(ip, port, interfaceMode, username, password)
+	case "http":
+		fmt.Println("HTTP not implemented yet")
+	case "forward":
+
+	case "interface-master":
+		ip, port := ArgumentParser.ParseInterfaceArguments()
+		Interface.Server(ip, port)
+	case "help":
+		ArgumentParser.ShowGeneralHelpMessage()
+	default:
+		_, _ = fmt.Fprintln(os.Stderr, "Unknown command\nTry: ", os.Args[0], " help")
+		os.Exit(-1)
 	}
 }
