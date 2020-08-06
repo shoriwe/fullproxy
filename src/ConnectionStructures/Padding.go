604da0bcde0ad17122cd423e57d07a4904c2f835
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-08-06 13:00:07 -0500

    Create Padding.go

---
+++ src/ConnectionStructures/Padding.go
@@ -0,0 +1,21 @@
+package ConnectionStructures
+
+
+import (
+	"bytes"
+	"crypto/aes"
+)
+
+
+func PKCS7PaddingAES(ciphertext []byte) []byte {
+	padding := aes.BlockSize - len(ciphertext) % aes.BlockSize
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padText...)
+}
+
+
+func PKCS7UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unPadding := int(origData[length-1])
+	return origData[:(length - unPadding)]
+}
