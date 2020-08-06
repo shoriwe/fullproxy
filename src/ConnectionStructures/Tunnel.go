dedc500d8f1bbaa8149fa71298ae714b07b9bbeb
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-08-06 13:00:11 -0500

    Create Tunnel.go

---
+++ src/ConnectionStructures/Tunnel.go
@@ -0,0 +1,47 @@
+package ConnectionStructures
+
+import (
+	"crypto/cipher"
+	"fmt"
+)
+
+
+type TunnelReader struct {
+	CipherBlock cipher.Block
+	ActiveSocketReader SocketReader
+}
+
+
+func (tunnelReader *TunnelReader) Read(buffer []byte) (int,  error){
+	tempBuffer := make([]byte, len(buffer))
+	numberOfReceivedBytes, connectionError := tunnelReader.ActiveSocketReader.Read(tempBuffer)
+	if connectionError == nil{
+		decryptedBufferPadded := make([]byte, len(tempBuffer))
+		tunnelReader.CipherBlock.Decrypt(decryptedBufferPadded, tempBuffer)
+		unPaddedBuffer := PKCS7UnPadding(decryptedBufferPadded[:numberOfReceivedBytes])
+		copy(buffer, decryptedBufferPadded[:numberOfReceivedBytes])
+		return len(unPaddedBuffer), connectionError
+	}
+	return numberOfReceivedBytes, connectionError
+}
+
+
+type TunnelWriter struct {
+	CipherBlock cipher.Block
+	ActiveSocketWriter SocketWriter
+}
+
+
+func (tunnelWriter *TunnelWriter) Write(buffer []byte) (int, error){
+	paddedBuffer := PKCS7PaddingAES(buffer)
+	encryptedBuffer := make([]byte, len(paddedBuffer))
+	fmt.Print(encryptedBuffer)
+	tunnelWriter.CipherBlock.Encrypt(encryptedBuffer, paddedBuffer)
+	return tunnelWriter.ActiveSocketWriter.Write(encryptedBuffer)
+}
+
+func (tunnelWriter *TunnelWriter) Flush() error {
+	return tunnelWriter.ActiveSocketWriter.Flush()
+}
+
+
