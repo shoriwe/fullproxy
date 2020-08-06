a8bf9a914ed9d53b71c5ea7f8b947d29991c4b8b
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-08-06 13:00:26 -0500

    Update BindCommand.go

--- src/Proxies/SOCKS5/BindCommand.go
+++ src/Proxies/SOCKS5/BindCommand.go
@@ -1,13 +1,12 @@
 package SOCKS5
 
 import (
-	"bufio"
 	"net"
 )
 
 
-func PrepareBind(clientConnection net.Conn, clientConnectionReader *bufio.Reader,
-clientConnectionWriter *bufio.Writer, targetAddress *string,
+func PrepareBind(clientConnection net.Conn, clientConnectionReader interface{},
+clientConnectionWriter interface{}, targetAddress *string,
 targetPort *string, targetAddressType *byte){
 	_ = clientConnection.Close()
 }
