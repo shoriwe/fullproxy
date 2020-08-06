5e85f198e2b3b49788ed936cc65205b1d20cd549
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-08-06 13:00:40 -0500

    Update SocketTools.go

--- src/Sockets/SocketTools.go
+++ src/Sockets/SocketTools.go
@@ -1,24 +1,22 @@
 package Sockets
 
 import (
-	"bufio"
+	"github.com/shoriwe/FullProxy/src/ConnectionStructures"
 	"net"
 )
 
 
-func CreateReaderWriter(connection net.Conn) (*bufio.Reader, *bufio.Writer){
-	return bufio.NewReader(connection), bufio.NewWriter(connection)
-}
-
-
-func Send(connectionWriter *bufio.Writer, data *[]byte) (int, error){
-	NumberOfBytesSent, ConnectionError := connectionWriter.Write(*data)
+func Send(connectionWriter ConnectionStructures.SocketWriter, data *[]byte) (int, error){
+	var NumberOfBytesSent int
+	var ConnectionError error
+	NumberOfBytesSent, ConnectionError = connectionWriter.Write(*data)
 	_ = connectionWriter.Flush()
 	return NumberOfBytesSent, ConnectionError
 }
 
 
-func Receive(connectionReader *bufio.Reader, bufferSize int) (int, []byte, error){
+func Receive(connectionReader ConnectionStructures.SocketReader, bufferSize int) (int, []byte, error){
+	var receivedBytesError error
 	buffer := make([]byte, bufferSize)
 	NumberOfReceivedBytes, receivedBytesError := connectionReader.Read(buffer)
 	if receivedBytesError != nil{
