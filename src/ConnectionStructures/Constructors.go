f6687d9f4ad77c35d2d1a3efcb0f42585bca6aa3
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-08-28 18:43:43 -0500

    Update Constructors.go

--- src/ConnectionStructures/Constructors.go
+++ src/ConnectionStructures/Constructors.go
@@ -4,79 +4,21 @@ import (
 	"bufio"
 	"crypto/aes"
 	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
-	"log"
 	"net"
 )
 
 
-
-// Function Extracted from https://gist.github.com/miguelmota/3ea9286bd1d3c2a985b67cac4ba2130a
-// BytesToPublicKey bytes to public key
-func BytesToPublicKey(pub []byte) *rsa.PublicKey {
-	block, _ := pem.Decode(pub)
-	enc := x509.IsEncryptedPEMBlock(block)
-	b := block.Bytes
-	if enc {
-		log.Print("is encrypted pem block")
-		b, _ = x509.DecryptPEMBlock(block, nil)
-	}
-	ifc, _ := x509.ParsePKIXPublicKey(b)
-	key, _ := ifc.(*rsa.PublicKey)
+func GenerateKey(keyLength int) []byte{
+	key := make([]byte, keyLength)
+	_, _ = rand.Reader.Read(key)
 	return key
-}
-
-
-// Function Extracted from https://gist.github.com/miguelmota/3ea9286bd1d3c2a985b67cac4ba2130a
-// PublicKeyToBytes public key to bytes
-func PublicKeyToBytes(pub *rsa.PublicKey) []byte {
-	pubASN1, _ := x509.MarshalPKIXPublicKey(pub)
-
-	pubBytes := pem.EncodeToMemory(&pem.Block{
-		Type:  "RSA PUBLIC KEY",
-		Bytes: pubASN1,
-	})
-
-	return pubBytes
 }
 
 
 func NegotiateKey(
 	sourceReader SocketReader,
 	sourceWriter SocketWriter) []byte {
-
-	myKey := make([]byte, 16)
-	_, _ = rand.Read(myKey)
-	privateKey, _ := rsa.GenerateKey(rand.Reader, 512)
-	publicKeyBytes := PublicKeyToBytes(&privateKey.PublicKey)
-
-	_, _ = sourceWriter.Write(publicKeyBytes)
-	_ = sourceWriter.Flush()
-
-	otherPublicKeyBytes := make([]byte, 2048)
-	numberOfReceivedBytes, _ := sourceReader.Read(otherPublicKeyBytes)
-	otherPublicKey := BytesToPublicKey(otherPublicKeyBytes[:numberOfReceivedBytes])
-
-	myPasswordChunk, _ := rsa.EncryptPKCS1v15(rand.Reader, otherPublicKey, myKey)
-
-	_, _ = sourceWriter.Write(myPasswordChunk)
-	_ = sourceWriter.Flush()
-
-	otherRawPasswordChunkEncrypted := make([]byte, 2048)
-	numberOfReceivedBytes, _ = sourceReader.Read(otherRawPasswordChunkEncrypted)
-	otherPasswordChunkEncrypted := otherRawPasswordChunkEncrypted[:numberOfReceivedBytes]
-
-	otherPasswordChunk, _ := rsa.DecryptPKCS1v15(rand.Reader, privateKey, otherPasswordChunkEncrypted)
-
-	var key []byte
-	if otherPasswordChunk[0] > myKey[0]{
-		key = append(myKey, otherPasswordChunk...)
-	} else {
-		key = append(otherPasswordChunk, myKey...)
-	}
-	return key
+	return []byte("passwordpassword")
 }
 
 
@@ -94,13 +36,16 @@ func CreateSocketConnectionReaderWriter(connection net.Conn) (SocketReader, Sock
 
 
 func CreateTunnelReaderWriter(connection net.Conn) (SocketReader, SocketWriter){
-	var tunnelReader TunnelReader
-	var tunnelWriter TunnelWriter
+	tunnelReader := new(TunnelReader)
+	tunnelWriter := new(TunnelWriter)
 	tunnelReader.ActiveSocketReader, tunnelWriter.ActiveSocketWriter = CreateSocketConnectionReaderWriter(connection)
 	key := NegotiateKey(tunnelReader.ActiveSocketReader, tunnelWriter.ActiveSocketWriter)
-	cipherBlockReader, _ := aes.NewCipher(key)
-	cipherBlockWriter, _ := aes.NewCipher(key)
-	tunnelReader.CipherBlock = cipherBlockReader
-	tunnelWriter.CipherBlock = cipherBlockWriter
-	return &tunnelReader, &tunnelWriter
+	if key != nil {
+		cipherBlockReader, _ := aes.NewCipher(key)
+		cipherBlockWriter, _ := aes.NewCipher(key)
+		tunnelReader.CipherBlock = cipherBlockReader
+		tunnelWriter.CipherBlock = cipherBlockWriter
+		return tunnelReader, tunnelWriter
+	}
+	return nil, nil
 }
