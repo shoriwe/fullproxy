63cd1799b5c30c96e3a3bceae127888014d1fb9a
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-09-25 10:43:45 -0500

    Update HashAlgorithms.go

--- src/Hashing/HashAlgorithms.go
+++ src/Hashing/HashAlgorithms.go
@@ -4,22 +4,19 @@ import (
 	"crypto/sha512"
 )
 
-
-func Sha3_512_256(buffer []byte) []byte{
+func Sha3_512_256(buffer []byte) []byte {
 	return sha512.New512_256().Sum(buffer)
 }
 
-
-func GetPasswordHashPasswordByteArray(username *[]byte, password *[]byte) []byte{
-	if len(*username) != 0 && len(*password) != 0{
+func GetPasswordHashPasswordByteArray(username *[]byte, password *[]byte) []byte {
+	if len(*username) != 0 && len(*password) != 0 {
 		return Sha3_512_256(*password)
 	}
 	return []byte{}
 }
 
-
-func GetPasswordHashPasswordString(username *string, password *string) []byte{
-	if len(*username) > 0{
+func GetPasswordHashPasswordString(username *string, password *string) []byte {
+	if len(*username) > 0 {
 		return Sha3_512_256([]byte(*password))
 	}
 	return []byte{}
