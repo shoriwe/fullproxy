3dbf0f54da90d937c38b1e091b3bba8b5da2204e
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-09-25 10:42:00 -0500

    Update IPTools.go

--- src/Sockets/IPTools.go
+++ src/Sockets/IPTools.go
@@ -2,12 +2,11 @@ package Sockets
 
 import "strconv"
 
-
-func GetIPv6(rawIPv6Address []byte) string{
+func GetIPv6(rawIPv6Address []byte) string {
 	ipv6 := ""
-	for index := 0; index < 16; index++{
+	for index := 0; index < 16; index++ {
 		ipv6 += strconv.FormatInt(int64(rawIPv6Address[index]), 16)
-		if (index != 15) && (index != 0) && (index % 2 != 0){
+		if (index != 15) && (index != 0) && (index%2 != 0) {
 			ipv6 += ":"
 		}
 	}
