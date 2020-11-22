4439913c40faae974d37b3ce22ef910c95235aef
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-11-22 15:08:16 -0500

    Support for the interface implementation

--- pkg/Proxies/PortForward/Remote.go
+++ pkg/Proxies/PortForward/Remote.go
@@ -1,13 +1,53 @@
 package PortForward
 
 import (
+	"bufio"
+	"crypto/tls"
 	"fmt"
-	"github.com/shoriwe/FullProxy/pkg/MasterSlave"
+	"github.com/shoriwe/FullProxy/pkg/ConnectionHandlers"
+	"github.com/shoriwe/FullProxy/pkg/Proxies/Basic"
+	"github.com/shoriwe/FullProxy/pkg/Sockets"
+	"net"
 )
+
+type RemoteForward struct {
+	MasterHost string
+	MasterPort string
+	TLSConfiguration *tls.Config
+}
+
+func (remoteForward *RemoteForward)Handle(
+	clientConnection net.Conn,
+	clientConnectionReader *bufio.Reader,
+	clientConnectionWriter *bufio.Writer) error {
+	targetConnection, connectionError := Sockets.TLSConnect(
+		&remoteForward.MasterHost,
+		&remoteForward.MasterPort,
+		(*remoteForward).TLSConfiguration)
+	if connectionError == nil {
+		targetConnectionReader, targetConnectionWriter := Sockets.CreateSocketConnectionReaderWriter(targetConnection)
+		portProxy := Basic.PortProxy{
+			TargetConnection: targetConnection,
+			TargetConnectionReader: targetConnectionReader,
+			TargetConnectionWriter: targetConnectionWriter,
+		}
+		return portProxy.Handle(clientConnection, clientConnectionReader, clientConnectionWriter)
+	}
+	_ = clientConnection.Close()
+	return connectionError
+}
+
+func (remoteForward *RemoteForward)SetAuthenticationMethod(_ ConnectionHandlers.AuthenticationFunction) error {
+	return nil
+}
 
 func StartRemotePortForward(localAddress *string, localPort *string, masterAddress *string, masterPort *string) {
 	if !(*localAddress == "" || *localPort == "" || *masterAddress == "" || *masterPort == "") {
-		MasterSlave.RemotePortForwardSlave(masterAddress, masterPort, localAddress, localPort)
+		remoteForward := RemoteForward{
+			MasterHost: *localAddress,
+			MasterPort: *localPort,
+		}
+		ConnectionHandlers.RemotePortForwardSlave(masterAddress, masterPort, localAddress, localPort, &remoteForward)
 	} else {
 		fmt.Println("All flags need to be in use")
 	}
