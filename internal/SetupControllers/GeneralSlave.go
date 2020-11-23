4274e73530c4d896b3302cff9dc7c114775c7561
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-11-22 19:56:45 -0500

    SetupControllers
    Tools to start the proxying service

---
+++ internal/SetupControllers/GeneralSlave.go
@@ -0,0 +1,30 @@
+package SetupControllers
+
+import (
+	"github.com/shoriwe/FullProxy/pkg/ConnectionControllers"
+	"github.com/shoriwe/FullProxy/pkg/ConnectionControllers/Slave"
+	"github.com/shoriwe/FullProxy/pkg/Sockets"
+	"log"
+)
+
+func GeneralSlave(host *string, port *string, proxy ConnectionControllers.ProxyProtocol) {
+	masterConnection, connectionError := Sockets.Connect(host, port)
+	if connectionError != nil {
+		log.Fatal(connectionError)
+	}
+	tlsConfiguration, creationError := Sockets.CreateSlaveTLSConfiguration()
+	if creationError != nil {
+		log.Fatal(creationError)
+	}
+	masterConnection = Sockets.UpgradeClientToTLS(masterConnection, tlsConfiguration)
+	masterConnectionReader, masterConnectionWriter := Sockets.CreateSocketConnectionReaderWriter(masterConnection)
+	controller := new(Slave.General)
+	controller.MasterConnection = masterConnection
+	controller.TLSConfiguration = tlsConfiguration
+	controller.MasterConnectionReader = masterConnectionReader
+	controller.MasterConnectionWriter = masterConnectionWriter
+	controller.MasterHost = *host
+	controller.MasterPort = *port
+	controller.ProxyProtocol = proxy
+	log.Fatal(controller.Serve())
+}
---
+++ internal/SetupControllers/RemotePortForwardSlave.go
@@ -0,0 +1,35 @@
+package SetupControllers
+
+import (
+	"github.com/shoriwe/FullProxy/pkg/ConnectionControllers/Slave"
+	"github.com/shoriwe/FullProxy/pkg/Sockets"
+	"log"
+)
+
+func RemotePortForwardSlave(
+	socks5Host *string, socks5Port *string,
+	bindHost *string, bindPort *string) {
+	tlsConfiguration, configurationError := Sockets.CreateSlaveTLSConfiguration()
+	if configurationError != nil {
+		log.Fatal(configurationError)
+	}
+	masterConnection, connectionError := Sockets.Connect(socks5Host, socks5Port)
+	if connectionError != nil {
+		log.Fatal(connectionError)
+	}
+	localServer, bindError := Sockets.Bind(bindHost, bindPort)
+	if bindError != nil {
+		log.Fatal(bindError)
+	}
+	masterConnection = Sockets.UpgradeClientToTLS(masterConnection, tlsConfiguration)
+	masterConnectionReader, masterConnectionWriter := Sockets.CreateSocketConnectionReaderWriter(masterConnection)
+	controller := new(Slave.RemotePortForward)
+	controller.MasterHost = *socks5Host
+	controller.MasterPort = *socks5Port
+	controller.TLSConfiguration = tlsConfiguration
+	controller.LocalServer = localServer
+	controller.MasterConnection = masterConnection
+	controller.MasterConnectionReader = masterConnectionReader
+	controller.MasterConnectionWriter = masterConnectionWriter
+	log.Fatal(controller.Serve())
+}
