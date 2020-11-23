587fb91ed4ed711a34f31c8d4cad8345a016233a
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-11-23 02:41:09 -0500

    Log features interfaces

--- pkg/ConnectionControllers/Constants.go
+++ pkg/ConnectionControllers/Constants.go
@@ -2,7 +2,7 @@ package ConnectionControllers
 
 import (
 	"bufio"
-	"github.com/shoriwe/FullProxy/pkg/Proxies/Basic"
+	"github.com/shoriwe/FullProxy/pkg/Proxies/PortProxy"
 	"github.com/shoriwe/FullProxy/pkg/Sockets"
 	"net"
 )
@@ -14,13 +14,23 @@ var (
 	UnknownOperation      = []byte{4}
 )
 
-type ConnectionController interface {
-	Serve() error
+func LogData(loggingMethod LoggingMethod, arguments...interface{}) {
+	if loggingMethod != nil {
+		loggingMethod(arguments...)
+	}
 }
 
 type AuthenticationMethod func(username []byte, password []byte) bool
 
+type LoggingMethod func(args...interface{})
+
+type ConnectionController interface {
+	SetLoggingMethod(LoggingMethod)
+	Serve() error
+}
+
 type ProxyProtocol interface {
+	SetLoggingMethod(LoggingMethod) error
 	SetAuthenticationMethod(AuthenticationMethod) error
 	Handle(net.Conn, *bufio.Reader, *bufio.Writer) error
 }
@@ -29,7 +39,7 @@ func StartGeneralProxying(clientConnection net.Conn, targetConnection net.Conn) 
 	clientConnectionReader, clientConnectionWriter := Sockets.CreateSocketConnectionReaderWriter(clientConnection)
 	targetConnectionReader, targetConnectionWriter := Sockets.CreateSocketConnectionReaderWriter(targetConnection)
 	if targetConnectionReader != nil && targetConnectionWriter != nil {
-		portProxy := Basic.PortProxy{
+		portProxy := PortProxy.PortProxy{
 			TargetConnection:       targetConnection,
 			TargetConnectionReader: targetConnectionReader,
 			TargetConnectionWriter: targetConnectionWriter,
