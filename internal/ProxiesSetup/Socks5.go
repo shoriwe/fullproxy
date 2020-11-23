70acb6ebbc0255be62acc7fb8692bcc655e89ed1
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-11-22 19:57:43 -0500

    Update Socks5.go

--- internal/ProxiesSetup/Socks5.go
+++ internal/ProxiesSetup/Socks5.go
@@ -1,23 +1,22 @@
 package ProxiesSetup
 
 import (
-	"github.com/shoriwe/FullProxy/pkg/ConnectionHandlers"
-	"github.com/shoriwe/FullProxy/pkg/ConnectionHandlers/Slave"
+	"github.com/shoriwe/FullProxy/internal/SetupControllers"
 	"github.com/shoriwe/FullProxy/pkg/Proxies/SOCKS5"
 )
 
-func SetupSocks5(host *string, port *string, slave *bool, username *[]byte, password *[]byte) {
+func SetupSocks5(host *string, port *string, slave *bool, username []byte, password []byte) {
 	proxy := new(SOCKS5.Socks5)
-	if len(*username) > 0 && len(*password) > 0 {
-		proxy.WantedAuthMethod = SOCKS5.BasicNegotiation
+	if len(username) > 0 && len(password) > 0 {
+		proxy.WantedAuthMethod = SOCKS5.UsernamePassword
 		proxy.SetAuthenticationMethod(BasicAuthentication(username, password))
 	} else {
 		proxy.WantedAuthMethod = SOCKS5.NoAuthRequired
 		proxy.SetAuthenticationMethod(NoAuthentication)
 	}
 	if *slave {
-		Slave.GeneralSlave(host, port, proxy)
+		SetupControllers.GeneralSlave(host, port, proxy)
 	} else {
-		ConnectionHandlers.Bind(host, port, proxy)
+		SetupControllers.Bind(host, port, proxy)
 	}
 }
