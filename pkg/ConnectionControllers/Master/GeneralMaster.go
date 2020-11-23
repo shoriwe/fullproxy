83419637008720b56fda3f16e496261648ab8209
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-11-23 02:41:23 -0500

    Controllers log features

--- pkg/ConnectionControllers/Master/GeneralMaster.go
+++ pkg/ConnectionControllers/Master/GeneralMaster.go
@@ -16,6 +16,12 @@ type General struct {
 	MasterHost             string
 	TLSConfiguration       *tls.Config
 	Server                 net.Listener
+	LoggingMethod ConnectionControllers.LoggingMethod
+}
+
+func (general *General) SetLoggingMethod(loggingMethod ConnectionControllers.LoggingMethod) error {
+	general.LoggingMethod = loggingMethod
+	return nil
 }
 
 func (general *General) Serve() error {
@@ -26,6 +32,7 @@ func (general *General) Serve() error {
 			finalError = connectionError
 			break
 		}
+		ConnectionControllers.LogData(general.LoggingMethod, "Client connection received from: ", clientConnection.RemoteAddr().String())
 		_, connectionError = Sockets.Send(general.MasterConnectionWriter, &ConnectionControllers.NewConnection)
 		if connectionError != nil {
 			finalError = connectionError
@@ -34,13 +41,18 @@ func (general *General) Serve() error {
 
 		targetConnection, connectionError := general.Server.Accept()
 		if connectionError != nil {
+			ConnectionControllers.LogData(general.LoggingMethod, connectionError)
 			continue
 		}
 		// Verify that the new connection is also from the slave
 		if strings.Split(targetConnection.RemoteAddr().String(), ":")[0] == general.MasterHost {
+			ConnectionControllers.LogData(general.LoggingMethod, "Target connection received from: ", targetConnection.RemoteAddr().String())
 			targetConnection = Sockets.UpgradeServerToTLS(targetConnection, general.TLSConfiguration)
 			go ConnectionControllers.StartGeneralProxying(clientConnection, targetConnection)
+		} else {
+			ConnectionControllers.LogData(general.LoggingMethod, "(Ignoring) Connection received from a non slave client: ", targetConnection.RemoteAddr().String())
 		}
 	}
+	ConnectionControllers.LogData(general.LoggingMethod, finalError)
 	return finalError
 }
--- pkg/ConnectionControllers/Slave/RemotePortForwardSlave.go
+++ pkg/ConnectionControllers/Slave/RemotePortForwardSlave.go
@@ -3,9 +3,9 @@ package Slave
 import (
 	"bufio"
 	"crypto/tls"
+	"errors"
 	"github.com/shoriwe/FullProxy/pkg/ConnectionControllers"
 	"github.com/shoriwe/FullProxy/pkg/Sockets"
-	"log"
 	"net"
 	"time"
 )
@@ -18,6 +18,12 @@ type RemotePortForward struct {
 	MasterHost             string
 	MasterPort             string
 	TLSConfiguration       *tls.Config
+	LoggingMethod ConnectionControllers.LoggingMethod
+}
+
+func (remotePortForward *RemotePortForward) SetLoggingMethod(loggingMethod ConnectionControllers.LoggingMethod) error {
+	remotePortForward.LoggingMethod = loggingMethod
+	return nil
 }
 
 func (remotePortForward *RemotePortForward) Serve() error {
@@ -28,6 +34,7 @@ func (remotePortForward *RemotePortForward) Serve() error {
 			finalError = connectionError
 			break
 		}
+		ConnectionControllers.LogData(remotePortForward.LoggingMethod, "Client connection received from: ", clientConnection.RemoteAddr().String())
 		_, connectionError = Sockets.Send(remotePortForward.MasterConnectionWriter, &ConnectionControllers.NewConnection)
 		if connectionError != nil {
 			if parsedConnectionError, ok := connectionError.(net.Error); !(ok && parsedConnectionError.Timeout()) {
@@ -38,6 +45,7 @@ func (remotePortForward *RemotePortForward) Serve() error {
 		_ = remotePortForward.MasterConnection.SetReadDeadline(time.Now().Add(3 * time.Second))
 		numberOfBytesReceived, response, connectionError := Sockets.Receive(remotePortForward.MasterConnectionReader, 1)
 		if connectionError != nil {
+			ConnectionControllers.LogData(remotePortForward.LoggingMethod, connectionError)
 			continue
 		}
 		if numberOfBytesReceived != 1 {
@@ -53,16 +61,17 @@ func (remotePortForward *RemotePortForward) Serve() error {
 				go ConnectionControllers.StartGeneralProxying(clientConnection, targetConnection)
 			} else {
 				_ = clientConnection.Close()
-				log.Fatal("Connectivity issues with master server")
+				ConnectionControllers.LogData(remotePortForward.LoggingMethod, "Connectivity issues with master server")
+				return connectionError
 			}
 		case ConnectionControllers.FailToConnectToTarget[0]:
 			_ = clientConnection.Close()
-			log.Print("Something goes wrong when master connected to target")
-			break
+			ConnectionControllers.LogData(remotePortForward.LoggingMethod, "Something goes wrong when master connected to target")
+			return errors.New("Something goes wrong when master connected to target")
 		case ConnectionControllers.UnknownOperation[0]:
 			_ = clientConnection.Close()
-			log.Print("The master did not understood the message")
-			break
+			ConnectionControllers.LogData(remotePortForward.LoggingMethod, "The master did not understood the message")
+			return errors.New("The master did not understood the message")
 		}
 	}
 	return finalError
