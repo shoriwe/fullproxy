c3cd6db0acca136d44597abc789568a8a9e21907
Antonio <antoniojosedonishung@gmail.com>
2023-05-21 20:25:05 -0500

    fix: increased coverage in tls crypto generation

--- utils/crypto/tls.go
+++ utils/crypto/tls.go
@@ -6,20 +6,13 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
-	"log"
 	"math/big"
 	"time"
 )
 
 func DefaultTLSConfig() *tls.Config {
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		log.Panicf("Failed to generate private key: %v", err)
-	}
-	sN, sNErr := rand.Int(rand.Reader, big.NewInt(0xFF_FF_FF_FF_FF_FF_FF))
-	if sNErr != nil {
-		log.Panicf("invalid serial number %v", sNErr)
-	}
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	sN, _ := rand.Int(rand.Reader, big.NewInt(0xFF_FF_FF_FF_FF_FF_FF))
 	template := &x509.Certificate{
 		SerialNumber:          sN,
 		NotBefore:             time.Now(),
@@ -28,10 +21,7 @@ func DefaultTLSConfig() *tls.Config {
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
 		BasicConstraintsValid: true,
 	}
-	certDER, err := x509.CreateCertificate(rand.Reader, template, template, &privateKey.PublicKey, privateKey)
-	if err != nil {
-		log.Panicf("Failed to create certificate: %v", err)
-	}
+	certDER, _ := x509.CreateCertificate(rand.Reader, template, template, &privateKey.PublicKey, privateKey)
 	privateKeyPEM := &pem.Block{
 		Type:  "RSA PRIVATE KEY",
 		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
@@ -42,10 +32,7 @@ func DefaultTLSConfig() *tls.Config {
 	}
 	privateKeyPEMBytes := pem.EncodeToMemory(privateKeyPEM)
 	certPEMBytes := pem.EncodeToMemory(certPEM)
-	tlsCert, err := tls.X509KeyPair(certPEMBytes, privateKeyPEMBytes)
-	if err != nil {
-		log.Panicf("Failed to create TLS certificate: %v", err)
-	}
+	tlsCert, _ := tls.X509KeyPair(certPEMBytes, privateKeyPEMBytes)
 	return &tls.Config{
 		Certificates:       []tls.Certificate{tlsCert},
 		InsecureSkipVerify: true,
