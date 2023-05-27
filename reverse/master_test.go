5bed6dcb9370cb106134cc0f80b1d2b41df038bd
Antonio <antoniojosedonishung@gmail.com>
2023-05-27 12:14:07 -0500

    fix: complete master coverage

--- reverse/master_test.go
+++ reverse/master_test.go
@@ -1,14 +1,11 @@
 package reverse
 
 import (
+	"net"
 	"testing"
 
 	"github.com/shoriwe/fullproxy/v3/utils/network"
 	"github.com/stretchr/testify/assert"
-)
-
-const (
-	testMessage = "MESSAGE"
 )
 
 func TestMaster_init(t *testing.T) {
@@ -99,18 +96,19 @@ func TestMaster_SlaveDial(t *testing.T) {
 		defer service.Close()
 		doneChan := make(chan struct{}, 2)
 		defer close(doneChan)
+		testMessage := []byte("TEST")
 		go func() {
 			c, err := service.Accept()
 			assert.Nil(tt, err)
 			defer c.Close()
-			_, err = c.Write([]byte(testMessage))
+			_, err = c.Write(testMessage)
 			assert.Nil(tt, err)
 			<-doneChan
 		}()
 		go func() {
-			slave := &Slave{Master: master}
-			defer slave.Close()
-			go slave.Serve()
+			s := &Slave{Master: master}
+			defer s.Close()
+			go s.Serve()
 			<-doneChan
 		}()
 		m := &Master{
@@ -124,7 +122,7 @@ func TestMaster_SlaveDial(t *testing.T) {
 		buffer := make([]byte, len(testMessage))
 		_, rErr := serviceConn.Read(buffer)
 		assert.Nil(tt, rErr)
-		assert.Equal(tt, testMessage, string(buffer))
+		assert.Equal(tt, testMessage, buffer)
 		doneChan <- struct{}{}
 		doneChan <- struct{}{}
 	})
@@ -153,5 +151,39 @@ func TestMaster_SlaveDial(t *testing.T) {
 		_, dialErr := m.SlaveDial("tcp", service.Addr().String())
 		assert.NotNil(tt, dialErr)
 		doneChan <- struct{}{}
+	})
+}
+
+func TestMaster_SlaveAccept(t *testing.T) {
+	t.Run("Succeed", func(tt *testing.T) {
+		data := network.ListenAny()
+		defer data.Close()
+		control := network.ListenAny()
+		defer control.Close()
+		master := network.Dial(control.Addr().String())
+		defer master.Close()
+		sListener := network.ListenAny() // Slave listener
+		defer sListener.Close()
+		doneChan := make(chan struct{}, 2)
+		defer close(doneChan)
+		go func() {
+			s := &Slave{Master: master, Listener: sListener}
+			defer s.Close()
+			go s.Serve()
+			<-doneChan
+		}()
+		m := &Master{
+			Data:    data,
+			Control: control,
+		}
+		go func() {
+			conn, dErr := net.Dial(sListener.Addr().Network(), sListener.Addr().String())
+			assert.Nil(tt, dErr)
+			defer conn.Close()
+		}()
+		defer m.Close()
+		client, dialErr := m.SlaveAccept()
+		assert.Nil(tt, dialErr)
+		defer client.Close()
 	})
 }
