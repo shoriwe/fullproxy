638e88157174556882e1a3a0b3ee2ea449baa6bd
Antonio <antoniojosedonishung@gmail.com>
2023-06-11 16:27:48 -0500

    feat: compose support

---
+++ compose/compose_test.go
@@ -0,0 +1,66 @@
+package compose
+
+import (
+	"testing"
+
+	"github.com/shoriwe/fullproxy/v3/utils/network"
+)
+
+func Test_startServices(t *testing.T) {
+	service := network.ListenAny()
+	defer service.Close()
+	exampleName := "example"
+	p := &Proxy{
+		Type:    ProxyForward,
+		Network: new(string),
+		Address: new(string),
+		Listener: Network{
+			Type:    NetworkBasic,
+			Network: new(string),
+			Address: new(string),
+		},
+	}
+	*p.Network = service.Addr().Network()
+	*p.Address = service.Addr().String()
+	*p.Listener.Network = "tcp"
+	*p.Listener.Address = "localhost:0"
+	c := Compose{
+		Proxies: map[string]*Proxy{
+			exampleName: p,
+		},
+	}
+	errCh := make(chan error, 1)
+	go startServices(c.Proxies, errCh)
+	for p.Listener.listener == nil {
+	}
+	defer p.Listener.listener.Close()
+}
+
+func TestCompose_Start(t *testing.T) {
+	service := network.ListenAny()
+	defer service.Close()
+	exampleName := "example"
+	p := &Proxy{
+		Type:    ProxyForward,
+		Network: new(string),
+		Address: new(string),
+		Listener: Network{
+			Type:    NetworkBasic,
+			Network: new(string),
+			Address: new(string),
+		},
+	}
+	*p.Network = service.Addr().Network()
+	*p.Address = service.Addr().String()
+	*p.Listener.Network = "tcp"
+	*p.Listener.Address = "localhost:0"
+	c := Compose{
+		Proxies: map[string]*Proxy{
+			exampleName: p,
+		},
+	}
+	go c.Start()
+	for p.Listener.listener == nil {
+	}
+	defer p.Listener.listener.Close()
+}
